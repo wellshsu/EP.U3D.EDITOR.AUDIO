cfb9fb9cf61dc5584ac613959b2aa0ed7f6a5249
wellshsu <wellshsu@outlook.com>
2022-06-20 11:10:07 +0800

    initial commit

---
+++ Editor/AudioItem_Editor.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioItem_Editor : ScriptableWizard
+{
+    public static AudioItem_Editor Instance;
+
+    public static AudioCategory curCat;
+
+    void OnEnable()
+    {
+        Instance = this;
+    }
+
+    void OnDisable()
+    {
+        Instance = null;
+    }
+
+    private static string SearchStr = "";
+
+    private Action<string> callBack;
+
+    private List<string> contents;
+
+    private string curStr;
+
+    private Vector2 scroll = Vector2.zero;
+
+    private bool reScroll;
+
+    private float lastClickTime;
+
+    void OnGUI()
+    {
+        EditorGUIUtility.labelWidth = 80f;
+        string oldStr = SearchStr;
+        SearchStr = SearchField(SearchStr);
+        if (oldStr != SearchStr)
+        {
+            reScroll = true;
+        }
+        if (contents != null)
+        {
+            bool close = false;
+            var searchs =
+                contents.FindAll(item => item.IndexOf(SearchStr, StringComparison.OrdinalIgnoreCase) >= 0);
+            if (reScroll)
+            {
+                reScroll = false;
+                scroll = Vector2.up * ((EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing * 2) * searchs.IndexOf(curStr));
+                //Debug.Log(scroll);
+            }
+            scroll = GUILayout.BeginScrollView(scroll);
+            for (int i = 0; i < searchs.Count; i++)
+            {
+                Color oldColor = GUI.backgroundColor;
+                if (string.Equals(curStr, searchs[i], StringComparison.Ordinal))
+                {
+                    GUI.backgroundColor = Color.red;
+                }
+                if (GUILayout.Button(searchs[i]))
+                {
+                    if (!string.Equals(curStr, searchs[i], StringComparison.Ordinal))
+                    {
+                        curStr = searchs[i];
+                        if (callBack != null)
+                        {
+                            callBack(searchs[i]);
+                        }
+                    }
+                    else
+                    {
+                        if (Time.realtimeSinceStartup - lastClickTime < 0.5f)
+                        {
+                            close = true;
+                        }
+                    }
+                    lastClickTime = Time.realtimeSinceStartup;
+                }
+                GUI.backgroundColor = oldColor;
+            }
+            GUILayout.EndScrollView();
+            if (close)
+            {
+                Close();
+            }
+        }
+    }
+
+    public static void Invoke(AudioCategory cat, string[] values, int curIndex, Action<string> callBack)
+    {
+        if (Instance == null)
+        {
+            DisplayWizard<AudioItem_Editor>("Select Audio Item");
+        }
+
+        if (Instance != null)
+        {
+            curCat = cat;
+            Instance.contents = new List<string>(values);
+            Instance.contents.Sort();
+            Instance.callBack = callBack;
+            Instance.curStr = curIndex > 0 && curIndex < values.Length ? values[curIndex] : "";
+        }
+    }
+
+    public static void ChangeScroll()
+    {
+        if (Instance != null)
+            Instance.reScroll = true;
+    }
+
+    private static string SearchField(string value, params GUILayoutOption[] options)
+    {
+        MethodInfo info = typeof(EditorGUILayout).GetMethod("ToolbarSearchField",
+            BindingFlags.NonPublic | BindingFlags.Static, null, new[] { typeof(string), typeof(GUILayoutOption[]) },
+            null);
+        if (info != null)
+        {
+            value = (string)info.Invoke(null, new object[] { value, options });
+        }
+        return value;
+    }
+}
---
+++ Editor/BuildAudio.cs
@@ -0,0 +1,106 @@
+﻿//---------------------------------------------------------------------//
+//                    GNU GENERAL PUBLIC LICENSE                       //
+//                       Version 2, June 1991                          //
+//                                                                     //
+// Copyright (C) Wells Hsu, wellshsu@outlook.com, All rights reserved. //
+// Everyone is permitted to copy and distribute verbatim copies        //
+// of this license document, but changing it is not allowed.           //
+//                  SEE LICENSE.md FOR MORE DETAILS.                   //
+//---------------------------------------------------------------------//
+using EP.U3D.EDITOR.BASE;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace EP.U3D.EDITOR.AUDIO
+{
+    public class BuildAudio
+    {
+        public static Type WorkerType = typeof(BuildAudio);
+
+        [MenuItem(Constants.MENU_PATCH_BUILD_AUDIO)]
+        public static void Invoke()
+        {
+            var worker = Activator.CreateInstance(WorkerType) as BuildAudio;
+            worker.Execute();
+        }
+
+        public virtual void Execute()
+        {
+            AudioController controller = AssetDatabase.LoadAssetAtPath<AudioController>(Constants.AUDIO_CONTROLLER);
+            string toast;
+            if (controller)
+            {
+                Dictionary<string, AudioItem> oldItems = new Dictionary<string, AudioItem>();
+                for (int i = 0; i < controller.AudioCategories.Length; i++)
+                {
+                    AudioCategory temp = controller.AudioCategories[i];
+                    if (temp != null)
+                    {
+                        for (int j = 0; j < temp.AudioItems.Length; j++)
+                        {
+                            AudioItem item = temp.AudioItems[j];
+                            if (item != null)
+                            {
+                                if (oldItems.ContainsKey(item.Name) == false)
+                                {
+                                    oldItems.Add(item.Name, item);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Dictionary<string, List<AudioItem>> dict = new Dictionary<string, List<AudioItem>>();
+                List<string> audios = new List<string>();
+                Helper.CollectAssets(Constants.AUDIO_WORKSPACE, audios);
+                for (int i = 0; i < audios.Count; i++)
+                {
+                    string file = audios[i];
+                    AudioClip clip = AssetDatabase.LoadAssetAtPath<AudioClip>(file);
+                    if (clip)
+                    {
+                        AudioItem item;
+                        oldItems.TryGetValue(clip.name, out item);
+                        if (item == null)
+                        {
+                            item = new AudioItem();
+                            item.Name = clip.name;
+                            item.subItems = new[] { new AudioSubItem() { Clip = clip } };
+                        }
+                        string cat = "OTHER";
+                        string[] strs = clip.name.Split("_");
+                        if (strs.Length > 1) cat = strs[0];
+                        List<AudioItem> list;
+                        if (dict.TryGetValue(cat, out list) == false)
+                        {
+                            list = new List<AudioItem>();
+                            dict.Add(cat, list);
+                        }
+                        list.Add(item);
+                    }
+                }
+
+                List<AudioCategory> cats = new List<AudioCategory>();
+                foreach (var kvp in dict)
+                {
+                    cats.Add(new AudioCategory(controller) { Name = kvp.Key, AudioItems = kvp.Value.ToArray() });
+                }
+                controller.AudioCategories = cats.ToArray();
+
+                PrefabUtility.SavePrefabAsset(controller.gameObject);
+                toast = "Build " + audios.Count + " Audio(s) Success.";
+            }
+            else
+            {
+                toast = "Build Audio Fail Caused by Nil AudioController.";
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            Helper.ShowToast(toast);
+            Helper.Log(toast);
+        }
+    }
+}
